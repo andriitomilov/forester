3c6397bd31b59d061bd26d8faba465d49ea501f9
andriitomilov <andriitomilov@ukr.net>
2022-10-22 20:26:49 +0300

    Fixed slider

---
+++ js/slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    // centerMode: true,
+  });
+});
+
+
