2775a2011b88037de6017798daa712bf7d334319
andriitomilov <andriitomilov@ukr.net>
2022-10-27 00:14:10 +0300

    add fixed up-button

--- js/slick.js
+++ js/slick.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  // slider
   $(".slider").slick({
     infinite: true,
     slidesToShow: 3,
@@ -7,6 +8,21 @@ $(document).ready(function(){
     autoplaySpeed: 3000,
     // centerMode: true,
   });
+
+
+  // scroll
+   const btn = $(".btn-scroll");
+   $(window).scroll(function () {
+     if ($(window).scrollTop() > 500) {
+       btn.addClass("show");
+     } else {
+       btn.removeClass("show");
+     }
+   });
+   btn.on("click", function (e) {
+     e.preventDefault();
+     $("html, body").animate({ scrollTop: 0 }, "400");
+   });
 });
 
 
